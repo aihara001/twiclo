60c28ec8979f231070a0f6e57da5def9480afb5a
takeshi aihara <vshine01@gmail.com>
2018-08-06 16:59:34 +0000

    test14 commit

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
 def contact_mail(twiclo)
  @twiclo = twiclo
  @user = User.find_by(id: @twiclo.user_id)
- mail to: "@User.email", subject: "ツイート確認メール"
+ mail to: "@user.email", subject: "ツイート確認メール"
 end
 end
