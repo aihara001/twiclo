8c28dc6f06d5ffb7c78dc68e24772308b57a8806
takeshi aihara <vshine01@gmail.com>
2018-08-06 13:41:09 +0000

    test12 commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'heroku-test-app-taihara.c9users.io' }
+  config.action_mailer.delivery_method = :letter_opener
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :contacts
   root 'sessions#new'
   resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
